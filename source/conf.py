9b498eb0dd44fb05eb9f59943eabf3d42f773fee
nikothomas <nikojb@gmail.com>
2024-07-19 10:50:45 -0700

    README changes

---
+++ source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+project = 'ctdfjorder'
+copyright = '2024, Nikolas Yanek-Chrones'
+author = 'Nikolas Yanek-Chrones'
+release = '0.1.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon',]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,14 @@
+ctdfjorder
+==========
+.. automodule:: ctdfjorder
+    :members:
+
+.. toctree::
+   :maxdepth: 3
+   :caption: Contents:
+
+Indices and tables
+==================
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
